adaaf5548f3810deef4d3f8069fcab97bcd9745e
Petr Triska <ptriska@cemm.oeaw.ac.at>
2022-05-30 18:08:00 +0200

    change port

--- config.py
+++ config.py
@@ -1,15 +1,28 @@
+## CONFIG FILE
 
 """
 title of your app
 """
 app_title = "" # string
 
+"""
+password protected - if set to True, a login form will
+appear when the app is loaded
+Passwords and usernames can be specified in the pwd.txt file
+"""
 
+use_pwd = False # boolean
 
 """
 Markup styled string with information displayed at the start up of the application
 """
-synopsis_text = "" # string
+synopsis_text = """
+This web-based application enables interactive visualization and analysis of SARS-CoV-2 sequencing data from wastewater treatment plants.
+The application can be easily customized through the configuration file. For customization of the configuration file you will need a geojson file containing shape of your
+catchment areas, a csv file containing basic information about catchment areas and sequencing data in the csv table.
+The software is written in python 3.8 and the web server runs on Plotly Dash.
+
+""" # string
 
 synopsis_header = "Synopsis" # string
 
@@ -18,7 +31,7 @@ synopsis_header = "Synopsis" # string
 """
 set port on which the application will be deployed
 """
-port = 81 # integer
+port = 8050 # integer
 
 """
 set to True if you want to perform debugging. Do not use in production.
@@ -34,6 +47,11 @@ geojson = "geo_data/plants_simplified_5.geojson" # path to geojson
 simplified geo json file with sampling areas
 """
 geojson_simplified = "geo_data/supersimple.geojson" # path to geojson
+
+"""
+id key in geojson. Must match id key in data files.
+"""
+feature_id_key = "properties.uwwcode" # string
 
 """
 set geo coordinates of the center point on a map
